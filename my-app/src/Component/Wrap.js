4353c4245713c7c62cdb855fceaafefec786e8fe
Alina-Ijaz <alinaijaz073@gmail.com>
2023-03-11 08:04:54 +0500

    Fix identation

--- my-app/src/Component/Wrap.js
+++ my-app/src/Component/Wrap.js
@@ -3,15 +3,15 @@ function Wrap() {
     return (<div className="content-container">
         <div className="row">
             <div className="left-panel box">
-            <div className="overlay">
-                <img src="./R.png" alt="image" width="100%" />
-            </div>
+                <div className="overlay">
+                    <img src="./R.png" alt="image" width="100%" />
+                </div>
             </div>
             <div className="middle-panel box">
                 <div className="sub-middle-pannel">
-                 <h3 style={{ color: "navy", marginTop: '-45px' }}>Alina Ijaz</h3>
+                    <h3 style={{ color: "navy", marginTop: '-45px' }}>Alina Ijaz</h3>
                 </div>
-                
+
             </div>
         </div>
     </div>)
