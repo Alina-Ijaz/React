6a6a13e3a9d640560ff3e0a46bc5fbba5dcd5cc2
Alina-Ijaz <alinaijaz073@gmail.com>
2023-03-21 08:23:47 +0500

    Remove extra css

---
+++ my-app/src/Component/Left.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Left() {
+  return (
+    <div
+      style={{
+        backgroundColor: 'navy',
+        width: '260px',
+        height: '695px',
+       
+       
+      }}
+    />
+  );
+}
+
+export default Left;
---
+++ my-app/src/Component/Right.js
@@ -0,0 +1,18 @@
+/*import React from 'react';
+
+function Right() {
+  return (
+    <div
+      style={{
+        backgroundColor: '#FAF9EC',
+        width: '260px',
+        height: '695px',
+        float: 'left',
+        
+      
+      }}
+    />
+  );
+}
+
+export default Right;*/
